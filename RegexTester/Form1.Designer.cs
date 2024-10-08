721375a2d90e5d9d73b79b529f4b5c91172549f4
J.Sullivan <energy.keeper@gmail.com>
2024-10-08 18:25:44 -0400

    Initial Commit

---
+++ RegexTester/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace RegexTester {
+    partial class Form1 {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose (bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent () {
+            this.lblRegex = new Label();
+            this.txtRegex = new TextBox();
+            this.lblInput = new Label();
+            this.txtInput = new TextBox();
+            this.button1 = new Button();
+            this.txtOutput = new TextBox();
+            this.button2 = new Button();
+            this.cbIgnoreCase = new CheckBox();
+            this.cbTimeout = new CheckBox();
+            this.txtTimeout = new TextBox();
+            this.label1 = new Label();
+            this.cbReplacement = new CheckBox();
+            this.txtReplacement = new TextBox();
+            this.SuspendLayout();
+            // 
+            // lblRegex
+            // 
+            this.lblRegex.AutoSize = true;
+            this.lblRegex.Location = new Point(66, 15);
+            this.lblRegex.Name = "lblRegex";
+            this.lblRegex.Size = new Size(39, 15);
+            this.lblRegex.TabIndex = 0;
+            this.lblRegex.Text = "Regex";
+            // 
+            // txtRegex
+            // 
+            this.txtRegex.Location = new Point(111, 12);
+            this.txtRegex.Name = "txtRegex";
+            this.txtRegex.Size = new Size(265, 23);
+            this.txtRegex.TabIndex = 1;
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Location = new Point(36, 44);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new Size(69, 15);
+            this.lblInput.TabIndex = 2;
+            this.lblInput.Text = "Input String";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new Point(111, 41);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new Size(265, 23);
+            this.txtInput.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new Point(239, 154);
+            this.button1.Name = "button1";
+            this.button1.Size = new Size(137, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Test Output";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += this.button1_Click;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new Point(14, 183);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new Size(362, 213);
+            this.txtOutput.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new Point(301, 402);
+            this.button2.Name = "button2";
+            this.button2.Size = new Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += this.button2_Click;
+            // 
+            // cbIgnoreCase
+            // 
+            this.cbIgnoreCase.AutoSize = true;
+            this.cbIgnoreCase.Checked = true;
+            this.cbIgnoreCase.CheckState = CheckState.Checked;
+            this.cbIgnoreCase.Location = new Point(111, 126);
+            this.cbIgnoreCase.Name = "cbIgnoreCase";
+            this.cbIgnoreCase.Size = new Size(88, 19);
+            this.cbIgnoreCase.TabIndex = 8;
+            this.cbIgnoreCase.Text = "Ignore Case";
+            this.cbIgnoreCase.UseVisualStyleBackColor = true;
+            this.cbIgnoreCase.CheckedChanged += this.cbIgnoreCase_CheckedChanged;
+            // 
+            // cbTimeout
+            // 
+            this.cbTimeout.AutoSize = true;
+            this.cbTimeout.Location = new Point(111, 101);
+            this.cbTimeout.Name = "cbTimeout";
+            this.cbTimeout.Size = new Size(92, 19);
+            this.cbTimeout.TabIndex = 9;
+            this.cbTimeout.Text = "Use Timeout";
+            this.cbTimeout.UseVisualStyleBackColor = true;
+            this.cbTimeout.CheckedChanged += this.cbTimeout_CheckedChanged;
+            // 
+            // txtTimeout
+            // 
+            this.txtTimeout.Enabled = false;
+            this.txtTimeout.Location = new Point(239, 99);
+            this.txtTimeout.Name = "txtTimeout";
+            this.txtTimeout.Size = new Size(80, 23);
+            this.txtTimeout.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new Point(325, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new Size(51, 15);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Seconds";
+            // 
+            // cbReplacement
+            // 
+            this.cbReplacement.AutoSize = true;
+            this.cbReplacement.Location = new Point(14, 72);
+            this.cbReplacement.Name = "cbReplacement";
+            this.cbReplacement.Size = new Size(95, 19);
+            this.cbReplacement.TabIndex = 12;
+            this.cbReplacement.Text = "Replace With";
+            this.cbReplacement.UseVisualStyleBackColor = true;
+            this.cbReplacement.CheckedChanged += this.cbReplacement_CheckedChanged;
+            // 
+            // txtReplacement
+            // 
+            this.txtReplacement.Enabled = false;
+            this.txtReplacement.Location = new Point(111, 70);
+            this.txtReplacement.Name = "txtReplacement";
+            this.txtReplacement.Size = new Size(265, 23);
+            this.txtReplacement.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new SizeF(7F, 15F);
+            this.AutoScaleMode = AutoScaleMode.Font;
+            this.ClientSize = new Size(388, 437);
+            this.Controls.Add(this.txtReplacement);
+            this.Controls.Add(this.cbReplacement);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTimeout);
+            this.Controls.Add(this.cbTimeout);
+            this.Controls.Add(this.cbIgnoreCase);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblInput);
+            this.Controls.Add(this.txtRegex);
+            this.Controls.Add(this.lblRegex);
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = SizeGripStyle.Hide;
+            this.Text = "Regex Tester";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblRegex;
+        private TextBox txtRegex;
+        private Label lblInput;
+        private TextBox txtInput;
+        private Button button1;
+        private TextBox txtOutput;
+        private Button button2;
+        private CheckBox cbIgnoreCase;
+        private CheckBox cbTimeout;
+        private TextBox txtTimeout;
+        private Label label1;
+        private CheckBox cbReplacement;
+        private TextBox txtReplacement;
+    }
+}
---
+++ RegexTester/Form1.cs
@@ -0,0 +1,84 @@
+using System.Text.RegularExpressions;
+
+namespace RegexTester {
+    public partial class Form1 : Form {
+        public Form1 () {
+            InitializeComponent();
+        }
+
+        private bool validateInputs () {
+            string pattern = txtRegex.Text;
+            string input = txtInput.Text;
+            if (String.IsNullOrEmpty(pattern)) {
+                lblRegex.ForeColor = Color.Red;
+                return false;
+            } else {
+                lblRegex.ForeColor = Color.Black;
+            }
+
+            if (String.IsNullOrEmpty(input)) {
+                lblInput.ForeColor = Color.Red;
+                return false;
+            } else {
+                lblInput.ForeColor = Color.Black;
+            }
+            return true;
+        }
+
+        private void button1_Click (object sender, EventArgs e) {
+            if (!validateInputs()) {
+                return;
+            }
+            string filteredOutput = txtInput.Text;
+            RegexOptions options = (cbIgnoreCase.Checked) ? RegexOptions.IgnoreCase : RegexOptions.None;
+            TimeSpan tsRegex = new TimeSpan(0);
+            if (cbTimeout.Checked) {
+                if(String.IsNullOrEmpty(txtTimeout.Text)) { txtTimeout.Text = "1"; }
+                string tmpTs = txtTimeout.Text;
+                int timespan = 0;
+                if (Int32.TryParse(tmpTs, out timespan)) {
+                    if (timespan <= 0) { timespan = 1; }
+                    tsRegex = new TimeSpan(0, 0, timespan);
+                }
+
+                tsRegex = new TimeSpan(0, 0, timespan);
+            } else {
+                tsRegex = new TimeSpan(0,0,1);
+            }
+            Match match = Regex.Match(filteredOutput, txtRegex.Text, options, tsRegex);
+            while (match.Success) {
+                if (cbReplacement.Checked) {
+                    filteredOutput = filteredOutput.Replace(match.Value, txtReplacement.Text);
+                } else {
+                    filteredOutput = filteredOutput.Replace(match.Value, String.Format("*{0}*", match.Value));
+                }
+                match = match.NextMatch();
+            }
+            txtOutput.Text += filteredOutput + "\r\n";
+        }
+
+        private void button2_Click (object sender, EventArgs e) {
+            txtOutput.Clear();
+        }
+
+        private void cbIgnoreCase_CheckedChanged (object sender, EventArgs e) {
+
+        }
+
+        private void cbTimeout_CheckedChanged (object sender, EventArgs e) {
+            if (((CheckBox)sender).Checked) {
+                txtTimeout.Enabled = true;
+            } else {
+                txtTimeout.Enabled = false;
+            }
+        }
+
+        private void cbReplacement_CheckedChanged (object sender, EventArgs e) {
+            if (cbReplacement.Checked) {
+                txtReplacement.Enabled = true;
+            } else {
+                txtReplacement.Enabled = false;
+            }
+        }
+    }
+}
